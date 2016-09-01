7059ba3c8952cf828eec00e887b7bdf5a8f766e8
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-09-01 10:53:50 -0400

    A quick little script to build the bootloader for TABs and copy the quad core files to the AOSP out directory

---
+++ make_ar6mx.sh
@@ -0,0 +1,10 @@
+# Quick little script to build the TAB series bootloader
+ARCH=arm
+CROSS_COMPILE=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+THREADS=8
+
+make -j$THREADS distclean ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+make -j$THREADS ar6mxqandroid_config ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+make -j$THREADS ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+cp -v --remove-destination u-boot.imx ../../../out/target/product/ar6mx/u-boot.imx
+cp -v --remove-destination u-boot.imx ../../../out/target/product/ar6mx/u-boot-6q.imx
